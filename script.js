f8111b9cb3fd5e8702dd29dc70e1dbd9f7d5a6e9
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:07:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var version = document.getElementById('version'); // checking if updated
-version.innerText = 'javascript 3.4'
+version.innerText = 'javascript 3.5'
 
 var video = document.getElementById('video'); // video streamed 
 var canvas = document.getElementById('canvas'); // hidden element 
@@ -62,18 +62,18 @@ startbutton.addEventListener(
 var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
 var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
 
+/*
 // testing the code 
 const worker = new Tesseract.createWorker();
 await worker.loadLanguage('eng');
-/*
 await worker.initialize();
 
 const { data: { text } } = await worker.recognize('logo.JPG');
 textRecognitionResults.innerText = text;
 
-*/ 
 // end the Tesseract 
 await worker.terminate();
+*/ 
 
 
 
