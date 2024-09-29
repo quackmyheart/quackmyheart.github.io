f2c94a3fe05c37d2e4d6f38e5bd8ff82faabfa19
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:31:31 -0400

    Update recognizeImage.js 1.1

--- recognizeImage.js
+++ recognizeImage.js
@@ -1,2 +1,19 @@
 var versionRecognize = document.getElementById('versionRecognize'); // checking if updated
-versionRecognize.innerText = 'javascript 1.0'
+versionRecognize.innerText = 'javascript 1.1'
+
+// text recognition time 
+var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
+var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
+
+/* 
+// code from the documentation 
+const { createWorker } = require('tesseract.js');
+
+const worker = await createWorker('eng');
+
+(async () => {
+  const { data: { text } } = await worker.recognize("logo.JPG");
+  console.log(text);
+  await worker.terminate();
+})();
+*/ 
