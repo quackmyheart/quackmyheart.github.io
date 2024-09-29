e8c33fc422d7862f68ab40172a2e7276cecc9ef6
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-28 23:17:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+  var video = document.getElementById('video'); // video streamed 
+  var canvas = document.getElementById('canvas'); // hidden element 
+  var photo = document.getElementById('photo'); // where the photo is displayed
+  var startbutton = document.getElementById('startbutton'); // button pressed to capture the photo 
