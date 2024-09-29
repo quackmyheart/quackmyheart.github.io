ba6280dccbc3b5fc32385ed2c44f0071ee7202df
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 03:20:44 -0400

    Update geminiThingsStoredHere.js 1.6

--- geminiThingsStoredHere.js
+++ geminiThingsStoredHere.js
@@ -1,6 +1,7 @@
 var ver_gemini = document.getElementById("versionGemini"); // checking if updated
-ver_gemini.innerText = 'javascript gemini 1.5'; 
+ver_gemini.innerText = 'javascript gemini 1.6'; 
 
+/* 
 // Make sure to include these imports:
 // import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
@@ -10,3 +11,4 @@ const prompt = "Write a story about a magic backpack.";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
+*/ 
