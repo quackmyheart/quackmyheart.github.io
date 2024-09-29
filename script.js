fc242422a1911a8bc35535873743dda8a17f9e1d
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-28 23:22:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-  var video = document.getElementById('video'); // video streamed 
-  var canvas = document.getElementById('canvas'); // hidden element 
-  var photo = document.getElementById('photo'); // where the photo is displayed
-  var startbutton = document.getElementById('startbutton'); // button pressed to capture the photo 
+var video = document.getElementById('video'); // video streamed 
+var canvas = document.getElementById('canvas'); // hidden element 
+var photo = document.getElementById('photo'); // where the photo is displayed
+var startbutton = document.getElementById('startbutton'); // button pressed to capture the photo 
+
+// get the media from the user's device 
+navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error("An error occurred");
+      });
+
