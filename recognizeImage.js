83b89f91db7b480b226eae9eceed6670793a0ab5
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:37:17 -0400

    Update recognizeImage.js

--- recognizeImage.js
+++ recognizeImage.js
@@ -5,15 +5,4 @@ versionRecognize.innerText = 'javascript 1.2';
 var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
 var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
 
-/* 
-// code from the documentation 
-const { createWorker } = require('tesseract.js');
 
-const worker = await createWorker('eng');
-
-(async () => {
-  const { data: { text } } = await worker.recognize("logo.JPG");
-  console.log(text);
-  await worker.terminate();
-})();
-*/ 
