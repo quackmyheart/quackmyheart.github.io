ff85fa199aed42443798dd8eb46f5b257d43524d
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 01:48:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 var version = document.getElementById('version'); // checking if updated
-version.innerText = 'javascript 2.7'
-
-var image_to_recognize = ""; // image to be recognized 
+version.innerText = 'javascript 2.8'
 
 var video = document.getElementById('video'); // video streamed 
 var canvas = document.getElementById('canvas'); // hidden element 
 canvas.style.visibility='hidden'; // makes canvas hidden 
 var photo = document.getElementById('photo'); // where the photo is displayed
 var startbutton = document.getElementById('startbutton'); // button pressed to capture the photo 
-
-var codeButton = document.getElementById('codeButton'); // button pressed to extract text out of image 
-var userCode = document.getElementById('result'); // where the code extracted from the image should be placed 
 
 // numbers for the size... arbitrary for now! 
 var width = 320; 
@@ -54,39 +49,10 @@ startbutton.addEventListener(
 
 // clear photo 
   function clearphoto() {
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        
-        const data = canvas.toDataURL("image/png");
-        // image_to_recognize = data; // put the image inside of the global variable
-        photo.setAttribute("src", data);
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
   }
-
-/* 
-// recognize the text 
-codeButton.addEventListener(
-  "click",
-  (ev) => {
-    recognizeText();
-    ev.preventDefault();
-  },
-  false,
-);
-
-// recognize the text 
-async function recognizeText() {
-
-      // initializing tesseract.js
-      const worker = new Tesseract.createWorker();
-      await worker.loadLanguage('eng');
-      await worker.initialize();
-
-      const { data: { text } } = await worker.recognize(image_to_recognize);
-      userCode.innerText = text; 
-
-      await worker.terminate();
-      
-}
-
-*/ 
