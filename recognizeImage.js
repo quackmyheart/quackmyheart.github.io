a6f5eac311f72df9fd790d2e82101c79cdca4c48
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:45:03 -0400

    Update recognizeImage.js 1.5

--- recognizeImage.js
+++ recognizeImage.js
@@ -1,8 +1,8 @@
 var verrec = document.getElementById('versionRecognize'); // checking if updated
-verrec.innerText = 'javascript 1.4';
+verrec.innerText = 'javascript 1.5';
 
 // text recognition time 
-//var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
+var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
 //var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
 
 
