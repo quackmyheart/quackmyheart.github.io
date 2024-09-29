37ad198e3b2fa155a505ef4cac570a3f3a906ecc
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:38:51 -0400

    Update recognizeImage.js 1.3

--- recognizeImage.js
+++ recognizeImage.js
@@ -1,8 +1,8 @@
 var versionRecognize = document.getElementById('versionRecognize'); // checking if updated
-versionRecognize.innerText = 'javascript 1.2';
+versionRecognize.innerText = 'javascript 1.3';
 
 // text recognition time 
-var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
-var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
+//var codeButton = document.getElementById('codeButton'); // button to be pressed to start the text recognition 
+//var textRecognitionResults = document.getElementById('result'); // where the text recognition results are to be displayed
 
 
