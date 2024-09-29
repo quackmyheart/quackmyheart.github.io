9a42b24aa1aebc58ce90a51743539749da0aeac1
quackmyheart <85214314+quackmyheart@users.noreply.github.com>
2024-09-29 02:43:03 -0400

    Update script.js 4.0 changed colon

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var version = document.getElementById('version'); // checking if updated
-version.innerText = 'javascript 3.9'
+version.innerText = 'javascript 4.0';
 
 var video = document.getElementById('video'); // video streamed 
 var canvas = document.getElementById('canvas'); // hidden element 
